8cfcfbae46b98de386d78b9d501e73397d87dd37
Kurtko10 <kurtkoaf@gmail.com>
2024-09-07 06:44:28 +0200

    api call

--- src/service/apiCalls.js
+++ src/service/apiCalls.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 //const API_URL = "http://localhost:3010"
-const API_URL = "https://bisque-porcupine-999787.hostingersite.com";
+const API_URL = process.env.REACT_APP_API_URL || "https://bisque-porcupine-999787.hostingersite.com";
 
 
 //------------------AUTH-------
